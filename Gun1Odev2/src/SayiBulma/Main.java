f34e0dc948f13c94c3ee6cfeda00c57cb3cb5d12
rothpern <48412651+rothpern@users.noreply.github.com>
2022-09-21 20:58:40 +0300

    Add files via upload

---
+++ Gun1Odev2/src/SayiBulma/Main.java
@@ -0,0 +1,32 @@
+package SayiBulma;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int[] sayilar = {1,2,5,7,9,0};
+		int aranacak = 0;
+		boolean isExist = false;
+		
+		for(int sayi:sayilar) {
+			
+			if(sayi == aranacak) {
+				
+				isExist = true;
+				break;
+				
+			} 
+		}
+
+		if(isExist) {
+			
+			System.out.println(aranacak + " sayısı listede vardır.");
+			
+		}else {
+			
+			System.out.println(aranacak + " sayısı listede yoktur.");
+			
+		}
+	}
+
+}
