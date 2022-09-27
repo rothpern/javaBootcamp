493228b0da65e2beee61db84539081533b3bef82
rothpern <48412651+rothpern@users.noreply.github.com>
2022-09-28 01:05:23 +0300

    Delete week02Homework2/src/lessons directory

--- week02Homework2/src/lessons/lesson27Classes/Lesson27Classes.java
+++
@@ -1,9 +0,0 @@
-package lessons.lesson27Classes;
-
-public class Lesson27Classes {
-
-	public static void main(String[] args) {
-		
-	}
-
-}
--- week02Homework2/src/lessons/lesson34Constructors/ProductManager.java
+++
@@ -1,9 +0,0 @@
-package lessons.lesson34Constructors;
-
-public class ProductManager {
-	public void add(Product product) {
-		
-		System.out.println("Product added " + product.getName());
-		
-	}
-}
